c04606df5c6b354b1dcac3f55fdbaae06b085e5b
hasanpir-dev <dj11hp@gmail.com>
2023-04-04 21:44:04 +0400

    v1

--- api/controllers/coin.js
+++ api/controllers/coin.js
@@ -16,6 +16,30 @@ const getSingleCoin = errorWrapper(async (req, res, next) => {
     data: coin,
   });
 });
+
+const getAllCoins = errorWrapper(async (req, res, next) => {
+  const query = {
+    $and: [
+      req.query.title ? { title: { $regex: req.query.title, $options: "i" } } : {},
+      req.query.year ? { year: { $gte: req.query.year.split(",")[0], $lte: req.query.year.split(",")[1] } } : {},
+      req.query.price ? { price: { $gte: req.query.price.split(",")[0], $lte: req.query.price.split(",")[1] } } : {},
+      req.query.country ? { country: { $regex: req.query.country, $options: "i" } } : {},
+      req.query.metal ? { metal: { $regex: req.query.metal, $options: "i" } } : {},
+      req.query.quality ? { quality: { $regex: req.query.quality, $options: "i" } } : {},
+    ],
+  };
+
+  const coins = await Coin.find(query);
+
+  res.status(200).json({
+    success: true,
+    coinsCount: coins.length,
+    data: coins,
+  });
+});
+
+
+
 const getAllCoinsByCategory = errorWrapper(async (req, res, next) => {
   const { category_id } = req.params;
 
@@ -30,8 +54,14 @@ const getAllCoinsByCategory = errorWrapper(async (req, res, next) => {
   } = req.query;
 
   if (title) query.title = { $regex: title, $options: "i" };
-  if (year) query.year = { $gte: year[0], $lte: year[1] };
-  if (price) query.price = { $gte: price[0], $lte: price[1] };
+  if (year) {
+    const [minYear, maxYear] = year.split(',');
+    query.year = { $gte: minYear, $lte: maxYear };
+  }
+  if (price) {
+    const [minPrice, maxPrice] = price.split(',');
+    query.price = { $gte: minPrice, $lte: maxPrice };
+  }
   if (country) query.country = { $regex: country, $options: "i" };
   if (metal) query.metal = { $regex: metal, $options: "i" };
   if (quality) query.quality = { $regex: quality, $options: "i" };
@@ -49,6 +79,8 @@ const getAllCoinsByCategory = errorWrapper(async (req, res, next) => {
     data: coins,
   });
 });
+
+
 
 
 const addCoin = errorWrapper(async (req, res, next) => {
@@ -141,6 +173,7 @@ const undoLikeCoin = errorWrapper(async (req, res, next) => {
 
 module.exports = {
   getSingleCoin,
+  getAllCoins,
   getAllCoinsByCategory,
   addCoin,
   editCoin,
--- api/routes/index.js
+++ api/routes/index.js
@@ -3,9 +3,11 @@ const express = require("express");
 const auth = require("./auth");
 const category = require("./category");
 const user = require("./user");
+const { getAllCoins } = require("../controllers/coin");
 
 const router = express.Router();
 
+router.get('/coins',getAllCoins)
 router.use("/auth", auth);
 // router.use("/admin", admin);
 router.use("/users", user);
